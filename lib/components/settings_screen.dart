603797c898716c67be494d9301304a03f308af90
ThomasNow Productions <codeportalmail@protonmail.com>
2025-10-06 06:30:13 +0200

    Fix errors

--- lib/components/settings_screen.dart
+++ lib/components/settings_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../services/settings_service.dart';
-import '../services/conversation_service.dart';
 import '../utils/logger.dart';
 
 /// Settings screen for app preferences and configuration
--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter/material.dart';
 import '../utils/logger.dart';
 import 'conversation_service.dart';
 
